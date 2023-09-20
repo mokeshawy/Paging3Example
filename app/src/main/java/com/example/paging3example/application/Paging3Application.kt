5782ce0bf4b6338d9343344a55393e8287f4dce2
mohamed keshawy <mokeshawy@gmail.com>
2023-09-20 12:11:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/paging3example/application/Paging3Application.kt
@@ -0,0 +1,8 @@
+package com.example.paging3example.application
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class Paging3Application : Application() {
+}
---
+++ app/src/main/java/com/example/paging3example/fragments/home_fargment/presentation/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.example.paging3example.fragments.home_fargment.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.paging3example.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private val binding by lazy { FragmentHomeBinding.inflate(layoutInflater) }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
